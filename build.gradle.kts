be35425c7f662b218016d0aeb8b50011d1ae5920
Shykial <szymon.jeziorski@outlook.com>
2024-11-25 17:14:53 +0100

    Configured GitHub packages publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,3 +32,16 @@ gradlePlugin {
         }
     }
 }
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Shykial/aoc-inputs-downloader")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_PACKAGES_TOKEN")
+            }
+        }
+    }
+}
